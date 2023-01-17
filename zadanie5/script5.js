a16b5619295fec37939041a73238e1a5340b2044
Aleksander Gorecki <core.autocrlf>
2023-01-17 21:59:06 +0100

    exercise 5 without 3rd point still count doesnt work

--- zadanie5/script5.js
+++ zadanie5/script5.js
@@ -11,13 +11,38 @@ const asyncAdd = async (a,b) => {
 
 //2
 
+var count=0;
+
 const asyncAddNotSpecifiedNumberOfArgs = async (...nums) => {
-    let sum = 0
+    let sum = 0;
     for (const num in nums) {
-        sum = await asyncAdd(sum,num);
+        sum = await asyncAdd(sum, num);
+        count++;
     }
+    alert(`amount of async operations ${counter}`)
     return sum
 }
 
-//3
+//4
 //-----------------------------------
+
+//3
+const measureTime = (func) => {
+    const start = performance.now();
+    func();
+    const end = performance.now();
+    return end - start;
+}
+
+const numbers = [];
+
+for(var i =0;i<=100;i++) {
+    numbers.push(Math.floor(Math.random() * 100))
+}
+
+const time = measureTime(async () => {
+    return await asyncAddNotSpecifiedNumberOfArgs(...numbers);
+});
+
+alert(`time: ${time}`);
+alert(`count: ${count}`);
