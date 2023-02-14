ed1fb73d8d901ecaa9206a39b587e0e994bb93bd
Aleksander Gorecki <core.autocrlf>
2023-02-14 20:13:23 +0100

    exercise 7 add and remove locations

--- zadanie7/script7.js
+++ zadanie7/script7.js
@@ -1,11 +1,14 @@
 let locations = []
 
-fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=", {
-    method: "POST",
-    headers: {'Content-Type': 'application/json'},
-}).then(res => {
-    alert(res);
-});
+document.getElementById('add').addEventListener('click', () => addLocation());
+document.getElementById('remove').addEventListener('click', () => removeLocation());
+
+// fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ee6edccd4af1923f2d719c8687bfffe3", {
+//     method: "POST",
+//     headers: {'Content-Type': 'application/json'},
+// }).then(res => {
+//     alert(res);
+// });
 
 for(let i = 0;i<locations.length;i++) {
     // fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=", {
@@ -14,4 +17,22 @@ for(let i = 0;i<locations.length;i++) {
     // }).then(res => {
     //     alert(res);
     // });
+}
+
+function addLocation() {
+    let place = document.getElementById('place').value;
+    if(place.length>0) {
+        locations.push(place);
+    } else {
+        alert("podano nieprawidlowa wartosc");
+    }
+}
+
+function removeLocation() {
+    let place = document.getElementById('place').value;
+    let index = locations.indexOf(place);
+    if(index !== -1) {
+        locations.splice(index,1);
+    }
+    alert(locations.toString());
 }
