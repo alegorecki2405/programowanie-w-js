f853276fec95ab208f4d8708e16f1267ffdca807
Aleksander Gorecki <core.autocrlf>
2023-02-15 01:22:35 +0100

    exercise 7 the rest

--- zadanie7/script7.js
+++ zadanie7/script7.js
@@ -2,20 +2,34 @@
 document.getElementById('add').addEventListener('click', () => addLocation());
 document.getElementById('remove').addEventListener('click', () => removeLocation());
 
-// fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ee6edccd4af1923f2d719c8687bfffe3", {
-//     method: "POST",
-//     headers: {'Content-Type': 'application/json'},
-// }).then(res => {
-//     alert(res);
-// });
+function pullWeather() {
+    const locations = JSON.parse(localStorage.getItem("locations")) || [];
 
-for(let i = 0;i<locations.length;i++) {
-    // fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=", {
-    //     method: "POST",
-    //     headers: {'Content-Type': 'application/json'},
-    // }).then(res => {
-    //     alert(res);
-    // });
+    for(let i = 0;i<locations.length;i++) {
+        postRequestAndCreateDiv(locations[i]);
+    }
+
+}
+
+pullWeather()
+
+async function postRequestAndCreateDiv(location) {
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=ee6edccd4af1923f2d719c8687bfffe3";
+    fetch(url)
+        .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+            console.log(data);
+            let temp = data["main"].temp;
+            let humidity = data["main"].humidity;
+            let icon = data.weather[0].icon;
+            const newDiv = document.createElement("div")
+            newDiv.className = "panel"
+            newDiv.innerHTML =  "<p> miasto : "+ location +"</p><p> temperatura : " + temp + "</p><p> wilgotnosc : " + humidity + "</p>";
+            const newImg = document.createElement("img");
+            newImg.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            newDiv.appendChild(newImg);
+            document.getElementById("wheather").appendChild(newDiv);
+        })
 }
 
 function addLocation() {
@@ -27,6 +41,7 @@ function addLocation() {
     } else {
         alert("podano nieprawidlowa wartosc");
     }
+    window.location.reload();
 }
 
 function removeLocation() {
@@ -37,5 +52,5 @@ function removeLocation() {
         locations.splice(index,1);
         localStorage.setItem('locations',JSON.stringify(locations));
     }
-    alert(locations.toString());
+    window.location.reload();
 }
